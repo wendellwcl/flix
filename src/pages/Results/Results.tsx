dbf996ede9751d9df40c5c8fa07235d59b4d7dd7
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-09-06 13:39:39 -0300

    feat: showing the results on the Results page

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 
 //Utils
@@ -7,30 +7,40 @@ import fetchTMDBconfig from "../../utils/fetchTMDBconfig";
 //Interfaces
 import { IMovie } from "../../interfaces/interfaces";
 
+import { MoviesContext } from "../../contexts/MoviesContext";
+
+//Components
+import MovieCardDefault from "../../components/MovieCardDefault/MovieCardDefault";
+
+//Style
+import style from "./Results.module.css";
+
 const Results = () => {
     const { query } = useParams();
+
+    const { setLoading } = useContext(MoviesContext);
 
     const [moviesList, setMoviesList] = useState<IMovie[]>();
 
     useEffect(() => {
         async function fetchMoviesList() {
+            setLoading(true);
+
             const data = await fetchTMDBconfig(`${query}&page=1`);
 
             setMoviesList(data.results);
+            setLoading(false);
         }
 
         fetchMoviesList();
     }, [query]);
 
     return (
-        <div>
-            {moviesList && (
-                <ul>
-                    {moviesList.map((movie) => (
-                        <li key={movie.id}>{movie.title}</li>
-                    ))}
-                </ul>
-            )}
+        <div className={style.moviesList_Container}>
+            {moviesList &&
+                moviesList.map((movie) => (
+                    <MovieCardDefault key={movie.id} movie={movie} />
+                ))}
         </div>
     );
 };
