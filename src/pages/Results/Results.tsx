b2bbe1c02c2f2dbf43f26a0817ae39ad296d5fe5
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-09-06 12:56:35 -0300

    feat: fetching the data on the Results page

--- src/pages/Results/Results.tsx
+++ src/pages/Results/Results.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+//Utils
+import fetchTMDBconfig from "../../utils/fetchTMDBconfig";
+
+//Interfaces
+import { IMovie } from "../../interfaces/interfaces";
 
 const Results = () => {
     const { query } = useParams();
 
-    return <div>Results: {query}</div>;
+    const [moviesList, setMoviesList] = useState<IMovie[]>();
+
+    useEffect(() => {
+        async function fetchMoviesList() {
+            const data = await fetchTMDBconfig(`${query}&page=1`);
+
+            setMoviesList(data.results);
+        }
+
+        fetchMoviesList();
+    }, [query]);
+
+    return (
+        <div>
+            {moviesList && (
+                <ul>
+                    {moviesList.map((movie) => (
+                        <li key={movie.id}>{movie.title}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
 };
 
 export default Results;
