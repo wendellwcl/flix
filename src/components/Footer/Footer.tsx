e3fb27966de1d522e7e07cc0e39a42818f91405e
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-09-23 15:48:19 -0300

    feat: css/scss changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 //Assets
 import tmbd_logo from "../../assets/img/tmdb_logo.svg";
+import freepik_logo from "../../assets/img/freepik-logo.png";
 
 //Style
 import style from "./Footer.module.css";
@@ -7,13 +8,27 @@ import style from "./Footer.module.css";
 const Footer = () => {
     return (
         <footer className={style.footer}>
-            <span className={style.credits}>Créditos:</span>
-            <div className={style.credits_tmdb}>
-                <a href="https://www.themoviedb.org" target="_blank">
-                    <img src={tmbd_logo} alt="API TMDB - The Movie DataBase" />
-                    <span>The Movie Database</span>
-                </a>
-            </div>
+            <span className={style.title}>Créditos:</span>
+            <span className={style.credits}>
+                <div className={style.credits_item}>
+                    <a href="https://www.themoviedb.org" target="_blank">
+                        <img
+                            src={tmbd_logo}
+                            alt="API TMDB - The Movie DataBase"
+                        />
+                        <span>The Movie Database</span>
+                    </a>
+                </div>
+                <div className={style.credits_item}>
+                    <a
+                        href="https://br.freepik.com/fotos-gratis/variedade-de-elementos-de-cinema-em-fundo-vermelho-com-espaco-de-copia_7089700.htm#query=cinema&position=10&from_view=search&track=sph"
+                        target="_blank"
+                    >
+                        <img src={freepik_logo} alt="freepik" />
+                        <span>Freepik</span>
+                    </a>
+                </div>
+            </span>
         </footer>
     );
 };
