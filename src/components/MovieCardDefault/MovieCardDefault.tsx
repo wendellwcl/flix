edb70ec165f3ac19291f277e1ebfae6f942d0215
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-10-02 16:10:10 -0300

    feat: MovieCardDefault component placeholder during loading

--- src/components/MovieCardDefault/MovieCardDefault.tsx
+++ src/components/MovieCardDefault/MovieCardDefault.tsx
@@ -1,5 +1,8 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
+
+//Assets
+import load_icon from "../../assets/img/load-icon.png";
 
 //Context
 import { MoviesContext } from "../../contexts/MoviesContext";
@@ -15,6 +18,8 @@ interface Props {
 }
 
 const MovieCardDefault = ({ movie }: Props) => {
+    const elRef = useRef<HTMLImageElement>(null);
+    const loadingRef = useRef<HTMLDivElement>(null);
     const { genres } = useContext(MoviesContext);
 
     const [movieGenre, setMovieGenre] = useState<string | null>(null);
@@ -31,18 +36,25 @@ const MovieCardDefault = ({ movie }: Props) => {
         }
     }, [movie]);
 
+    function handleImgLoad() {
+        loadingRef.current!.style.display = "none";
+        elRef.current!.style.display = "flex";
+    }
+
     return (
         <div className={style.card_container}>
             <div className={style.img_container}>
                 <Link to={`/details/${movie.id}`} relative="path">
-                    {movie.poster_path ? (
-                        <img
-                            src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-                            alt={movie.title}
-                        />
-                    ) : (
-                        <div className={style.null_img}></div>
-                    )}
+                    <div className={style.loading} ref={loadingRef}>
+                        <img src={load_icon} />
+                    </div>
+                    <img
+                        ref={elRef}
+                        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
+                        alt={movie.title}
+                        onLoad={handleImgLoad}
+                        onError={handleImgLoad}
+                    />
                 </Link>
             </div>
             <div className={style.info_container}>
