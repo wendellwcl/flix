ce067dec5140f7dd77d70fb310326d8e2e73ae65
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-10-04 23:46:00 -0300

    feat: structuring and styling the Details page

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -10,6 +10,9 @@ import { LoadingContext } from "../../contexts/LoadingContext";
 //Utils
 import fetchTMDBconfig from "../../utils/fetchTMDBconfig";
 
+//Styles
+import style from "./Details.module.css";
+
 const Details = () => {
     const { id } = useParams();
 
@@ -24,6 +27,7 @@ const Details = () => {
             );
 
             setMovieDetails(movieDetails);
+            console.log(movieDetails);
         }
 
         setLoading(true);
@@ -35,13 +39,69 @@ const Details = () => {
         <div>
             {movieDetails && (
                 <>
-                    <div>
-                        <div>{id}</div>
-                        <div>{movieDetails.title}</div>
-                        <div>{movieDetails.tagline}</div>
-                        <div>{movieDetails.overview}</div>
-                        <div>{movieDetails.runtime}</div>
-                        <div>{movieDetails.vote_average}</div>
+                    <div className={style.details_container}>
+                        <div className={style.details_body}>
+                            <div className={style.centralize}>
+                                <div className={style.details_img}>
+                                    <img
+                                        src={`https://image.tmdb.org/t/p/w400${movieDetails.poster_path}`}
+                                        alt={movieDetails.title}
+                                    />
+                                </div>
+                                <div className={style.details_infos}>
+                                    <h3 className={style.details_title}>
+                                        {movieDetails.title}
+                                    </h3>
+                                    {movieDetails.tagline && (
+                                        <h4 className={style.details_tagline}>
+                                            {movieDetails.tagline}
+                                        </h4>
+                                    )}
+                                    {movieDetails.overview && (
+                                        <p className={style.details_overview}>
+                                            {movieDetails.overview}
+                                        </p>
+                                    )}
+                                    {movieDetails.runtime && (
+                                        <p className={style.details_runtime}>
+                                            Duração:&nbsp;
+                                            <span>
+                                                {movieDetails.runtime} min
+                                            </span>
+                                        </p>
+                                    )}
+                                    {movieDetails.vote_average && (
+                                        <p className={style.details_average}>
+                                            Nota:&nbsp;
+                                            <span>
+                                                {movieDetails.vote_average}
+                                            </span>
+                                        </p>
+                                    )}
+                                    {movieDetails.genres.length > 0 && (
+                                        <p className={style.details_genres}>
+                                            Gêneros:&nbsp;
+                                            <span>
+                                                {movieDetails.genres.map(
+                                                    (genre: {
+                                                        name: string;
+                                                    }) => (
+                                                        <span
+                                                            className={
+                                                                style.genre
+                                                            }
+                                                        >
+                                                            {genre.name}
+                                                            &nbsp;&nbsp;
+                                                        </span>
+                                                    )
+                                                )}
+                                            </span>
+                                        </p>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <Footer />
                 </>
