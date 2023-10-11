c35a0b03eac5b41ccc466ef7cd538fc7750bbc89
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2023-10-11 13:41:45 -0300

    refactor: modifying "LoadScreen" visual

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,10 +1,13 @@
+//Assets
+import load_logo from "../../assets/img/load-icon.png";
+
 //Style
 import style from "./LoadingScreen.module.css";
 
 const LoadingScreen = () => {
     return (
         <div className={style.loading_container}>
-            <span>Carregando...</span>
+            <img src={load_logo} className={style.load_img} />
         </div>
     );
 };
